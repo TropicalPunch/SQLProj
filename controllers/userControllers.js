82cae517e6381fb632d69f2b10f69c6ced81b6a1
TropicalPunch <billabang1@hotmail.com>
2021-05-17 22:01:16 +0300

    user registration with hashed password in DB

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,38 +1,54 @@
-const User = require('../models/user');
-//models.User.create(newUser) for registration
+const models = require('../models');
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+
 
 const registerUser = async (req, res)=>{
-    const {email, password, name} = req.body;
-    //if userexists in DB...
-     const userExists = await User.findOne({email: email})
     
-    if(userExists){
-         res.status(400)
-         throw new Error('Cannot register, email exists in DB')
-    }
+    const {name, email, password} = req.body;
+    
+    //if userexists in DB...
+    models.User.findOne({ where: { email: email } }).then( result =>{
+        if(result){ //if user exists in db
+            res.status(409).json({
+                massage:'Cannot register, email exists in DB'
+            });
+               
+        }else{ //user does not exists in db , proceed ...
 
-    //else create it in the DB!
-
-    const newUser = await User.create({
-        name,
-        email,
-        password
-    })
-
-    if(newUser){
-        res.ststus(201).json({
-            //_id: user._id,
-            name: user.name,
-            email: user.email,
-            //token: generateToken(user._id) 
+            const newUser = {
+                name,
+                email,
+                password
+            };
+        
+            //hash the password before adding the user to the DB
+            const salt = bcrypt.genSaltSync(10);
+            newUser.password =  bcrypt.hashSync(password, salt); 
+            
+            //email does not exists in the db, we shall now create it!
+            models.User.create(newUser).then(result => {
+                res.status(201).json({
+                   message: 'user created!' 
+                })
+            }).catch(error =>{
+                res.status(400).json({
+                    message: 'User could not be created- invalid data'
+                })
+            })
+        
+        }
+        }).catch(error => {
+            res.status(500).json({
+                message: 'Cannot register, internal server error'
+            })
         })
-    }else{
-        res.status(400)
-        throw new Error('User could not be created- invalid data')
-    }
 
 }
 
 module.exports = {
-    registerUser
+    registerUser: registerUser,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
   
-import express from 'express'
+const express = require ('express');
 
 const router = express.Router()// api/users/
 
 const { registerUser } = require ('../controllers/userControllers.js'); 
 
-router.route('/').post(registerUser);
+router.route('/register').post(registerUser);
+
+module.exports = router;
